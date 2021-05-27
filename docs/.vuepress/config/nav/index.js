5fd5291fa18d2fcbfebd724309496ee0f76d19b2
mrhaoxiaojun <1458149969@qq.com>
2021-05-27 09:05:15 +0800

    brower

--- docs/.vuepress/config/nav/index.js
+++ docs/.vuepress/config/nav/index.js
@@ -5,13 +5,14 @@ module.exports = [
   {
     text: '教程笔记',
     items: [
-      { text: '面试知识库', link: '/course/interview/js' },
-      { text: 'Javascript设计模式-简版教程篇', link: '/course/designMode/intro' },
-      { text: 'Git 简版教程篇', link: '/course/git/intro' },
-      { text: 'Webpack 浅聊', link: '/course/webpack/' },
-      { text: 'ECMAScript 6 收录笔记', link: '/course/es6/read' },
+      { text: '面试知识库-待整理学习', link: '/course/interview/js' },
+      { text: 'Git 简版实战教程篇', link: '/course/git/intro' },
+      { text: 'Webpack 实战浅聊', link: '/course/webpack/' },
+      { text: '正则-简版实战教程篇-待完成', link: '/course/regExp/regExp' },
+      { text: '浏览器工作原理 100问', link: '/course/browser/' },
       { text: 'Linux实战-笔记篇', link: '/course/linux/linuxBase' },
-      { text: '正则-简版教程篇', link: '/course/regExp/regExp' },
+      { text: 'ECMAScript 6 收录笔记-未完成', link: '/course/es6/read' },
+      { text: 'Javascript设计模式-简版教程篇', link: '/course/designMode/intro' },
     ]
   },
   { text: '联系', 
